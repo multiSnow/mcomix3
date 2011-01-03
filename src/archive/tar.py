9fff47ce871bc716fc3dab14f990c4339340d059
oddegamra <oddegamra@users.sourceforge.net>
2011-01-03 19:18:12 +0000

    Introduce TAR archive wrapper
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@35 ef8490bc-f317-4725-81ed-e95a9aca1bcf

---
+++ src/archive/tar.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+
+""" Unicode-aware wrapper for zipfile.ZipFile. """
+
+import os
+import tarfile
+import archive_base
+
+class TarArchive(archive_base.NonUnicodeArchive):
+    def __init__(self, archive):
+        super(TarArchive, self).__init__(archive)
+        self.tar = tarfile.open(archive, 'r')
+
+    def list_contents(self):
+        return [self._unicode_filename(filename)
+           for filename in self.tar.getnames()]
+
+    def extract(self, filename, destination_path):
+        destination_dir = os.path.split(destination_path)[0]
+        self._create_directory(destination_path)
+
+        new = file(destination_path, 'wb')
+        file_object = self.tar.extractfile(self._original_filename(filename))
+        new.write(file_object.read())
+        file_object.close()
+        new.close()
+
+    def close(self):
+        self.tar.close()
+
+# vim: expandtab:sw=4:ts=4
