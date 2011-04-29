58764704ba8d8e2c42d4a7b965d8ed7d32d4afa4
oddegamra <oddegamra@users.sourceforge.net>
2011-04-29 10:02:41 +0000

    Remove redundant functions from portability.
    The get_*_directory functions are also in tools.
    
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@253 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/portability.py
+++ mcomix/portability.py
@@ -5,48 +5,6 @@ import sys
 import locale
 import ctypes
 
-def get_home_directory():
-    """On UNIX-like systems, this method will return the path of the home
-    directory, e.g. /home/username. On Windows, it will return a MComix
-    sub-directory of <Documents and Settings/Username>.
-    """
-    if sys.platform == 'win32':
-        return os.path.join(os.path.expanduser('~'), 'MComix')
-    else:
-        return os.path.expanduser('~')
-
-
-def get_config_directory():
-    """Return the path to the MComix config directory. On UNIX, this will
-    be $XDG_CONFIG_HOME/mcomix, on Windows it will be the same directory as
-    get_home_directory().
-
-    See http://standards.freedesktop.org/basedir-spec/latest/ for more
-    information on the $XDG_CONFIG_HOME environmental variable.
-    """
-    if sys.platform == 'win32':
-        return get_home_directory()
-    else:
-        base_path = os.getenv('XDG_CONFIG_HOME',
-            os.path.join(get_home_directory(), '.config'))
-        return os.path.join(base_path, 'mcomix')
-
-
-def get_data_directory():
-    """Return the path to the MComix data directory. On UNIX, this will
-    be $XDG_DATA_HOME/mcomix, on Windows it will be the same directory as
-    get_home_directory().
-
-    See http://standards.freedesktop.org/basedir-spec/latest/ for more
-    information on the $XDG_DATA_HOME environmental variable.
-    """
-    if sys.platform == 'win32':
-        return get_home_directory()
-    else:
-        base_path = os.getenv('XDG_DATA_HOME',
-            os.path.join(get_home_directory(), '.local/share'))
-        return os.path.join(base_path, 'mcomix')
-
 def uri_prefix():
     """ The prefix used for creating file URIs. This is 'file://' on
     Linux, but 'file:' on Windows due to urllib using a different
