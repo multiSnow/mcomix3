d8e68bed11bc88270a02e190298fb9e511d89638
oddegamra <oddegamra@users.sourceforge.net>
2011-10-09 12:32:48 +0000

    Fixed "Copy to clipboard" doing nothing on Win32.
    PyGTK's Clipboard class seems to have some problems with delayed
    clipboard data, so fall back on Win32 API to do the copying. Why is
    Win32 API so ugly?
    
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@456 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/clipboard.py
+++ mcomix/clipboard.py
@@ -1,6 +1,11 @@
 """clipboard.py - Clipboard handler"""
 
 import gtk
+import ctypes
+import cStringIO
+import sys
+
+import log
 import image_tools
 
 class Clipboard(gtk.Clipboard):
@@ -28,21 +33,81 @@ class Clipboard(gtk.Clipboard):
                         self._window.is_manga_mode )
 
             # Get path for current page
-            path = self._window.imagehandler.get_path_to_page().encode('utf-8')
+            path = self._window.imagehandler.get_path_to_page()
+            if sys.platform == 'win32':
+                self._copy_windows(pixbuf, path)
+            else:
+                self._copy_linux(pixbuf, path.encode('utf-8'))
 
-            # Register various clipboard formats for either the text or the pixbuf.
-            clipboard_targets = ("image/bmp", "text/plain", "STRING", "UTF8_STRING")
-            self.set_with_data(
-                [ (target, 0, 0) for target in clipboard_targets ],
-                self._get_clipboard_content,
-                self._clear_clipboard_content,
-                (pixbuf, path))
+    def _copy_windows(self, pixbuf, path):
+        """ Copies pixbuf and path to the clipboard.
+        Uses native Win32 API, as GTK+ doesn't seem to work. """
+
+        windll = ctypes.windll
+        OpenClipboard = windll.user32.OpenClipboard
+        EmptyClipboard = windll.user32.EmptyClipboard
+        SetClipboardData = windll.user32.SetClipboardData
+        CloseClipboard = windll.user32.CloseClipboard
+        GlobalAlloc = windll.kernel32.GlobalAlloc
+        GlobalLock = windll.kernel32.GlobalLock
+        GlobalLock.restype = ctypes.c_void_p
+        GlobalUnlock = windll.kernel32.GlobalUnlock
+
+        def buffer_to_handle(buffer, buffer_size):
+            """ Creates a memory handle for the passed data.
+            This handle doesn't need to be freed by the application. """
+            global_mem = GlobalAlloc(
+                0x0042, # GMEM_MOVEABLE | GMEM_ZEROINIT
+                buffer_size)
+            lock = GlobalLock(global_mem)
+            ctypes.memmove(lock, ctypes.addressof(buffer), buffer_size)
+            GlobalUnlock(global_mem)
+
+            return global_mem
+
+        # Paste the text as Unicode string
+        text_buffer = ctypes.create_unicode_buffer(path)
+        text_handle = buffer_to_handle(text_buffer,
+                ctypes.sizeof(text_buffer))
+        # Paste the image as Win32 DIB structure
+        pil = image_tools.pixbuf_to_pil(pixbuf)
+        output = cStringIO.StringIO()
+        pil.convert("RGB").save(output, "BMP")
+        dibdata = output.getvalue()[14:]
+        output.close()
+
+        image_buffer = ctypes.create_string_buffer(dibdata)
+        image_handle = buffer_to_handle(image_buffer,
+                ctypes.sizeof(image_buffer))
+
+        # Actually copy data to clipboard
+        if OpenClipboard(self._window.window.handle):
+            EmptyClipboard()
+            SetClipboardData(13, # CF_UNICODETEXT
+                text_handle)
+            SetClipboardData(8, # CF_DIB
+                image_handle)
+            CloseClipboard()
+        else:
+            log.warning('Could not open clipboard.')
+
+    def _copy_linux(self, pixbuf, path):
+        # Register various clipboard formats for either the
+        # text or the pixbuf.
+        clipboard_targets = ("image/bmp",
+                "text/plain", "STRING", "UTF8_STRING")
+        self.set_with_data(
+            [ (target, 0, 0) for target in clipboard_targets ],
+            self._get_clipboard_content,
+            self._clear_clipboard_content,
+            (pixbuf, path))
 
     def _get_clipboard_content(self, clipboard, selectiondata, info, data):
-        """ Called whenever an application requests the content of the clipboard.
-        selectiondata.target will contain one of the targets that have previously been
-        registered. Currently, only "image/bmp" provides pixbuf data, while all
-        other types point to the currently opened file as UTF-8 string. """
+        """ Called whenever an application requests the content of the
+        clipboard. selectiondata.target will contain one of the targets
+        that have previously been registered. Currently, only "image/bmp"
+        provides pixbuf data, while all other types point to the currently
+        opened file as UTF-8 string. """
 
         if selectiondata.target == "image/bmp":
             selectiondata.set_pixbuf(data[0])
