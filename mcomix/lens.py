aa333996351407ade724718a1208e6fcecdf7407
oddegamra <oddegamra@users.sourceforge.net>
2012-01-06 14:48:53 +0000

    Use original pixbuf instead of scaled one as source for lens pixbuf.
    (Closes bug #3470259)
    
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@527 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/lens.py
+++ mcomix/lens.py
@@ -126,8 +126,9 @@ class MagnifyingLens(object):
 
         if self._window.displayed_double():
 
-            l_source_pixbuf = self._window.left_image.get_pixbuf()
-            r_source_pixbuf = self._window.right_image.get_pixbuf()
+            l_source_pixbuf, r_source_pixbuf = self._window.imagehandler.get_pixbufs()
+            if self._window.is_manga_mode:
+                l_source_pixbuf, r_source_pixbuf = r_source_pixbuf, l_source_pixbuf
 
             l_image_size = self._window.left_image.size_request()
             r_image_size = self._window.right_image.size_request()
@@ -138,7 +139,7 @@ class MagnifyingLens(object):
                 l_image_size[0], left=False)
         else:
 
-            source_pixbuf = self._window.left_image.get_pixbuf()
+            source_pixbuf = self._window.imagehandler.get_pixbufs()[0]
 
             image_size = self._window.left_image.size_request()
             self._add_subpixbuf(canvas, x, y, image_size, source_pixbuf)
@@ -160,6 +161,10 @@ class MagnifyingLens(object):
         The image we are getting the coordinates for is the left one unless
         <left> is False.
         """
+        # Prevent division by zero exceptions further down
+        if not image_size[0]:
+            return
+
         area_x, area_y = self._window.get_visible_area_size()
         if left:
             padding_x = max(0,
@@ -172,16 +177,47 @@ class MagnifyingLens(object):
         x -= padding_x
         y -= padding_y
 
-        source_mag = prefs['lens magnification']
+        rotation = prefs['rotation']
+        if prefs['auto rotate from exif']:
+            rotation += image_tools.get_implied_rotation(source_pixbuf)
+            rotation = rotation % 360
+
+        if rotation in [90, 270]:
+            scale = float(source_pixbuf.get_height()) / image_size[0]
+        else:
+            scale = float(source_pixbuf.get_width()) / image_size[0]
+
+        x *= scale
+        y *= scale
+
+        source_mag = prefs['lens magnification'] / scale
         width = height = prefs['lens size'] / source_mag
 
-        paste_left = x > width // 2
-        paste_top = y > height // 2
+        paste_left = x > width / 2
+        paste_top = y > height / 2
         dest_x = max(0, int(math.ceil((width / 2 - x) * source_mag)))
         dest_y = max(0, int(math.ceil((height / 2 - y) * source_mag)))
 
-        src_x = x - width // 2
-        src_y = y - height // 2
+        if rotation == 90:
+            x, y = y, source_pixbuf.get_height() - x
+        elif rotation == 180:
+            x = source_pixbuf.get_width() - x
+            y = source_pixbuf.get_height() - y
+        elif rotation == 270:
+            x, y = source_pixbuf.get_width() - y, x
+        if prefs['horizontal flip']:
+            if rotation in (90, 270):
+                y = source_pixbuf.get_height() - y
+            else:
+                x = source_pixbuf.get_width() - x
+        if prefs['vertical flip']:
+            if rotation in (90, 270):
+                x = source_pixbuf.get_width() - x
+            else:
+                y = source_pixbuf.get_height() - y
+
+        src_x = x - width / 2
+        src_y = y - height / 2
         if src_x < 0:
             width += src_x
             src_x = 0
@@ -199,6 +235,20 @@ class MagnifyingLens(object):
             int(math.ceil(source_mag * subpixbuf.get_width())),
             int(math.ceil(source_mag * subpixbuf.get_height())),
             gtk.gdk.INTERP_TILES)
+
+        if rotation == 90:
+            subpixbuf = subpixbuf.rotate_simple(
+                gtk.gdk.PIXBUF_ROTATE_CLOCKWISE)
+        elif rotation == 180:
+            subpixbuf = subpixbuf.rotate_simple(
+                gtk.gdk.PIXBUF_ROTATE_UPSIDEDOWN)
+        elif rotation == 270:
+            subpixbuf = subpixbuf.rotate_simple(
+                gtk.gdk.PIXBUF_ROTATE_COUNTERCLOCKWISE)
+        if prefs['horizontal flip']:
+            subpixbuf = subpixbuf.flip(horizontal=True)
+        if prefs['vertical flip']:
+            subpixbuf = subpixbuf.flip(horizontal=False)
 
         if paste_left:
             dest_x = 0
