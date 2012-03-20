e1b6639512795f27c381150b29ddeac01c417c96
oddegamra <oddegamra@users.sourceforge.net>
2012-03-20 15:23:34 +0000

    Remove function call overhead from get_most_common_edge_colour.
    This should supposedly affect performance for auto-background color,
    but in truth, the increase was only marginal (less than 10%). I'm only
    committing this change because I would have hated having wasted my time.
    
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@634 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/image_tools.py
+++ mcomix/image_tools.py
@@ -154,26 +154,63 @@ def get_most_common_edge_colour(pixbufs, edge=2):
     doesn't work as expected together with get_pixels().
     """
 
-    def round_color(color, steps=10):
-        """ This rounds an RGB tuple <color> to the next nearest value,
-        i.e. 128, 83, 10 becomes 130, 85, 10 with <steps>=5. This compensates for
-        dirty colors where no clear dominating color can be made out. """
+    def group_colors(colors, steps=10):
+        """ This rounds a list of colors in C{colors} to the next nearest value,
+        i.e. 128, 83, 10 becomes 130, 85, 10 with C{steps}=5. This compensates for
+        dirty colors where no clear dominating color can be made out.
 
-        def round(color_value):
-            if steps % 2 == 0:
-                middle = steps // 2
+        @return: The color that appears most often in the prominent group."""
+
+        # Start group
+        group = (0, 0, 0)
+        # List of (count, color) pairs, group contains most colors
+        colors_in_prominent_group = []
+        color_count_in_prominent_group = 0
+        # List of (count, color) pairs, current color group
+        colors_in_group = []
+        color_count_in_group = 0
+
+        for count, color in colors:
+
+            # Round color
+            rounded = [0] * len(color)
+            for i, color_value in enumerate(color):
+                if steps % 2 == 0:
+                    middle = steps // 2
+                else:
+                    middle = steps // 2 + 1
+
+                remainder = color_value % steps
+                if remainder >= middle:
+                    color_value = color_value + (steps - remainder)
+                else:
+                    color_value = color_value - remainder
+
+                rounded[i] = min(255, max(0, color_value))
+
+            # Change prominent group if necessary
+            if rounded == group:
+                # Color still fits in the previous color group
+                colors_in_group.append((count, color))
+                color_count_in_group += count
             else:
-                middle = steps // 2 + 1
+                # Color group changed, check if current group has more colors
+                # than last group
+                if color_count_in_group > color_count_in_prominent_group:
+                    colors_in_prominent_group = colors_in_group
+                    color_count_in_prominent_group = color_count_in_group
 
-            remainder = color_value % steps
-            if remainder >= middle:
-                color_value = color_value + (steps - remainder)
-            else:
-                color_value = color_value - remainder
+                group = rounded
+                colors_in_group = [ (count, color) ]
+                color_count_in_group = count
 
-            return min(255, max(0, color_value))
+        # Cleanup if only one edge color group was found
+        if color_count_in_group > color_count_in_prominent_group:
+            colors_in_prominent_group = colors_in_group
 
-        return tuple(map(round, color))
+        colors_in_prominent_group.sort(key=operator.itemgetter(0), reverse=True)
+        # List is now sorted by color count, first color appears most often
+        return colors_in_prominent_group[0][1]
 
     def get_edge_pixbuf(pixbuf, side, edge):
         """ Returns a pixbuf corresponding to the side passed in <side>.
@@ -216,13 +253,8 @@ def get_most_common_edge_colour(pixbufs, edge=2):
 
     # Sum up colors from all edges
     ungrouped_colors.sort(key=operator.itemgetter(1))
-    colors = [ (sum(itertools.imap(operator.itemgetter(0), group)), color)
-        for color, group in itertools.groupby(ungrouped_colors, lambda t: round_color(t[1])) ]
-
-    # The most used color appears first in the list after it has been sorted
-    colors.sort(key=operator.itemgetter(0), reverse=True)
-    most_used = colors[0]
-    return [color * 257 for color in most_used[1]]
+    most_used = group_colors(ungrouped_colors)
+    return [color * 257 for color in most_used]
 
 def pil_to_pixbuf(image):
     """Return a pixbuf created from the PIL <image>."""
