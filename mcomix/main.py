c03c2573008830c6876947a046c03b7ef8ea6559
oddegamra <oddegamra@users.sourceforge.net>
2014-01-10 16:03:30 +0000

    Fixed 'Continue reading' not working for files from command line.
    From: Boris Bogar <eddie81@users.sf.net>
    
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@969 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/main.py
+++ mcomix/main.py
@@ -260,6 +260,7 @@ class MainWindow(gtk.Window):
         prefs['previous quit was quit and save'] = False
 
         if open_path is not None:
+            open_page = self.filehandler._get_last_read_page(open_path)
             self.filehandler.open_file(open_path, open_page)
 
         if is_slideshow:
