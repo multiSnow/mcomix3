2378fd01126b5c6ebb56f7338681d5d904b162a2
Benoit Pierre <benoit.pierre@gmail.com>
2015-03-13 21:07:33 +0000

    archive: fix external ZIP for glob like entries
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@1164 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/archive/zip_external.py
+++ mcomix/archive/zip_external.py
@@ -2,6 +2,7 @@
 
 """ ZIP archive extractor via executable."""
 
+from mcomix import i18n
 from mcomix import process
 from mcomix.archive import archive_base
 
@@ -32,5 +33,16 @@ class ZipExecArchive(archive_base.ExternalExecutableArchive):
     @staticmethod
     def is_available():
         return bool(ZipExecArchive._find_unzip_executable())
+
+    def _unicode_filename(self, filename, conversion_func=i18n.to_unicode):
+        unicode_name = conversion_func(filename)
+        safe_name = self._replace_invalid_filesystem_chars(unicode_name)
+        # As it turns out, unzip will try to interpret filenames as glob...
+        for c in '[*?':
+            filename = filename.replace(c, '[' + c + ']')
+        # Won't work on Windows...
+        filename = filename.replace('\\', '\\\\')
+        self.unicode_mapping[safe_name] = filename
+        return safe_name
 
 # vim: expandtab:sw=4:ts=4
