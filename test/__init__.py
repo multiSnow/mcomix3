d0073c904930f7644cdb942299de08c02eefc665
Benoit Pierre <benoit.pierre@gmail.com>
2015-03-13 21:07:57 +0000

    test: change __init__
    - set logging level to DEBUG
    - configure locale
    
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@1173 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
+
+# Configure locale.
+
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
+
 # Since some of MComix' modules depend on gettext being installed for _(),
 # add such a function here that simply returns the string passed into it.
 
 import __builtin__
+
 if '_' not in __builtin__.__dict__:
     __builtin__.__dict__['_'] = lambda text: unicode(text)
+
+# Enable debug logging to make post-mortem analysis easier.
+
+from mcomix import log
+
+log.setLevel('DEBUG')
+
