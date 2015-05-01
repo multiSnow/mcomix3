c9686bf0733596c081719d56ce5168e2c088d230
Benoit Pierre <benoit.pierre@gmail.com>
2015-05-01 20:48:32 +0000

    test/image tools: update rotation test
    Test PNG support.
    
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@1255 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- test/test_image_tools.py
+++ test/test_image_tools.py
@@ -10,10 +10,16 @@ class ImageToolsTest(unittest.TestCase):
 
     def test_implied_rotation(self):
         for image, rotation in (
+            # JPEG.
             ('landscape-exif-270-rotation.jpg', 270),
             ('landscape-no-exif.jpg'          , 0  ),
             ('portrait-exif-180-rotation.jpg' , 180),
             ('portrait-no-exif.jpg'           , 0  ),
+            # PNG.
+            ('landscape-exif-270-rotation.png', 270),
+            ('landscape-no-exif.png'          , 0  ),
+            ('portrait-exif-180-rotation.png' , 180),
+            ('portrait-no-exif.png'           , 0  ),
         ):
             pixbuf = image_tools.load_pixbuf(os.path.join('test', 'files', 'images', image))
             self.assertEqual(rotation, image_tools.get_implied_rotation(pixbuf))
