42201f206e3f781553f7dfe94c36db7bee128e4f
Benoit Pierre <benoit.pierre@gmail.com>
2015-05-01 20:49:09 +0000

    thumbnail view: rework API
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@1269 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/thumbnail_view.py
+++ mcomix/thumbnail_view.py
@@ -13,16 +13,18 @@ class ThumbnailViewBase(object):
     gtk.IconView. Instantiating this class directly is *impossible*,
     as it depends on methods provided by the view classes. """
 
-    def __init__(self, model):
+    def __init__(self, uid_column, pixbuf_column, status_column):
         """ Constructs a new ThumbnailView.
-        @param model: L{gtk.TreeModel} instance. The model needs a pixbuf
-                      and a boolean column for internal calculations.
+        @param uid_column: index of unique identifer column.
+        @param pixbuf_column: index of pixbuf column.
+        @param status_column: index of status boolean column
+                              (True if pixbuf is not temporary filler)
         """
 
-        #: Model index of the thumbnail status field (gobject.BOOLEAN)
-        self.status_column = -1
-        #: Model index of the pixbuf field
-        self.pixbuf_column = -1
+        #: Keep track of already generated thumbnails.
+        self._uid_column = uid_column
+        self._pixbuf_column = pixbuf_column
+        self._status_column = status_column
 
         #: Ignore updates when this flag is True.
         self._updates_stopped = True
@@ -32,14 +34,8 @@ class ThumbnailViewBase(object):
                                     unique_orders=True,
                                     max_threads=prefs["max threads"])
 
-    def generate_thumbnail(self, file_path, model, path):
-        """ This function must return the thumbnail for C{file_path}.
-        C{model} and {path} point at the relevant model line. """
-        raise NotImplementedError()
-
-    def get_file_path_from_model(self, model, iter):
-        """ This function should retrieve a file path from C{model},
-        the current row being specified by C{iter}. """
+    def generate_thumbnail(self, uid):
+        """ This function must return the thumbnail for C{uid}. """
         raise NotImplementedError()
 
     def get_visible_range(self):
@@ -61,9 +57,6 @@ class ThumbnailViewBase(object):
             # No valid paths available
             return
 
-        # Flush current pixmap generation orders.
-        self._thread.clear_orders()
-
         pixbufs_needed = []
         start = visible[0][0]
         end = visible[1][0]
@@ -73,31 +66,31 @@ class ThumbnailViewBase(object):
         required = range(start, end + additional + 1) + \
                    range(max(0, start - additional), start)
         model = self.get_model()
-        for path in required:
-            try:
+        # Filter invalid paths.
+        required = [path for path in required if 0 <= path < len(model)]
+        with self._thread:
+            # Flush current pixmap generation orders.
+            self._thread.clear_orders()
+            for path in required:
                 iter = model.get_iter(path)
-            except ValueError:
-                iter = None
-
-            # Do not queue again if cover was already created
-            if (iter is not None and
-                not model.get_value(iter, self.status_column)):
-
-                file_path = self.get_file_path_from_model(model, iter)
-                pixbufs_needed.append((file_path, path))
-
-        if len(pixbufs_needed) > 0:
-            self._updates_stopped = False
-            self._thread.extend_orders(pixbufs_needed)
+                uid, generated = model.get(iter,
+                                           self._uid_column,
+                                           self._status_column)
+                # Do not queue again if thumbnail was already created.
+                if not generated:
+                    pixbufs_needed.append((uid, iter))
+            if len(pixbufs_needed) > 0:
+                self._updates_stopped = False
+                self._thread.extend_orders(pixbufs_needed)
 
     def _pixbuf_worker(self, order):
         """ Run by a worker thread to generate the thumbnail for a path."""
-        file_path, path = order
-        pixbuf = self.generate_thumbnail(file_path, path)
+        uid, iter = order
+        pixbuf = self.generate_thumbnail(uid)
         if pixbuf is not None:
-            gobject.idle_add(self._pixbuf_finished, path, pixbuf)
+            gobject.idle_add(self._pixbuf_finished, iter, pixbuf)
 
-    def _pixbuf_finished(self, path, pixbuf):
+    def _pixbuf_finished(self, iter, pixbuf):
         """ Executed when a pixbuf was created, to actually insert the pixbuf
         into the view store. C{pixbuf_info} is a tuple containing
         (index, pixbuf). """
@@ -106,18 +99,17 @@ class ThumbnailViewBase(object):
             return 0
 
         model = self.get_model()
-        iter = model.get_iter(path)
-        model.set(iter, self.pixbuf_column, pixbuf)
-        # Mark as generated
-        model.set_value(iter, self.status_column, True)
+        model.set(iter, self._status_column, True, self._pixbuf_column, pixbuf)
 
         # Remove this idle handler.
         return 0
 
 class ThumbnailIconView(gtk.IconView, ThumbnailViewBase):
-    def __init__(self, model):
+    def __init__(self, model, uid_column, pixbuf_column, status_column):
+        assert gtk.TREE_MODEL_ITERS_PERSIST == (model.get_flags() & gtk.TREE_MODEL_ITERS_PERSIST)
         super(ThumbnailIconView, self).__init__(model)
-        ThumbnailViewBase.__init__(self, model)
+        ThumbnailViewBase.__init__(self, uid_column, pixbuf_column, status_column)
+        self.set_pixbuf_column(pixbuf_column)
 
         # Connect events
         self.connect('expose-event', self.draw_thumbnails_on_screen)
@@ -126,9 +118,10 @@ class ThumbnailIconView(gtk.IconView, ThumbnailViewBase):
         return gtk.IconView.get_visible_range(self)
 
 class ThumbnailTreeView(gtk.TreeView, ThumbnailViewBase):
-    def __init__(self, model):
+    def __init__(self, model, uid_column, pixbuf_column, status_column):
+        assert gtk.TREE_MODEL_ITERS_PERSIST == (model.get_flags() & gtk.TREE_MODEL_ITERS_PERSIST)
         super(ThumbnailTreeView, self).__init__(model)
-        ThumbnailViewBase.__init__(self, model)
+        ThumbnailViewBase.__init__(self, uid_column, pixbuf_column, status_column)
 
         # Connect events
         self.connect('expose-event', self.draw_thumbnails_on_screen)
