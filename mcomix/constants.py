78d0cbf94b3943deda4151a49891a96f30b8b53e
aaku <aaku@users.sourceforge.net>
2016-02-12 19:58:43 +0000

    MComix 1.2.1
    git-svn-id: https://svn.code.sf.net/p/mcomix/code@1450 ef8490bc-f317-4725-81ed-e95a9aca1bcf

--- mcomix/constants.py
+++ mcomix/constants.py
@@ -8,7 +8,7 @@ import operator
 from mcomix import tools
 
 APPNAME = 'MComix'
-VERSION = '1.3.dev0'
+VERSION = '1.2.1'
 
 HOME_DIR = tools.get_home_directory()
 CONFIG_DIR = tools.get_config_directory()
