f7fb4adc4c732c0eea049da975cb4a54899584ca
multiSnow <infinity.blick.winkel@gmail.com>
2019-03-16 01:03:29 +0800

    use argparse in comicthumb

--- mcomix/comicthumb.py
+++ mcomix/comicthumb.py
@@ -2,42 +2,61 @@
 
 '''comicthumb - Thumbnailer for comic book archives, bundled with MComix.
 
-comicthumb is dependent on the Python Imaging Library (PIL).
+comicthumb use same dependency with MComix.
 
 comicthumb was originally written by Christoph Wolk, this version was
 re-written from scratch for Comix 4 by Pontus Ekberg. 
-
-Supported formats: ZIP, RAR and tar (.cbz, .cbr, .cbt)
-
-Usage: comicthumb INFILE OUTFILE [SIZE]
 '''
+
+import argparse
+from os import getcwd
+from os.path import exists,join,normpath
+from urllib.parse import unquote
+
 from gi import require_version
 
 require_version('GdkPixbuf', '2.0')
 require_version('Gdk', '3.0')
 require_version('Gtk', '3.0')
 
+CURDIR=getcwd()
+
 from mcomix import thumbnail_tools
 from mcomix import portability
 
-import sys
-from urllib.parse import unquote
+THUMB_SIZE=128
+URL_PREFIX='file://'
 
-if __name__ == '__main__':
-    argv = sys.argv[1:]
+def abspath(path):
+    return normpath(join(CURDIR,path))
+
+def main():
+    parser=argparse.ArgumentParser(
+        prog='comicthumb',
+        description='Thumbnailer for comic book archives',
+        epilog='Supported formats: ZIP, RAR and tar (.cbz, .cbr, .cbt)',
+    )
+    parser.add_argument('infile',default=None,metavar='INFILE',
+                        help='input archive')
+    parser.add_argument('outfile',default=None,metavar='OUTFILE',
+                        help='output thumbnail')
+    parser.add_argument('size',nargs='?',default=THUMB_SIZE,metavar='SIZE',
+                        help='size of thumbnail (default: {})'.format(THUMB_SIZE))
+    ns=parser.parse_args()
+    in_path=abspath(ns.infile)
+    out_path=abspath(ns.outfile)
+    if in_path.startswith(URL_PREFIX):
+        in_path=unquote(in_path[len(URL_PREFIX):])
+    if not exists(in_path):
+        print('not exists:',ns.infile)
+        parser.print_usage()
+        return 1
     try:
-        in_path = argv[0]
-        out_path = argv[1]
-        if len(argv) == 3:
-            size = int(argv[2])
-        else:
-            size = 128
-    except:
-        print(__doc__)
-        sys.exit(1)
-
-    if in_path.startswith('file://'):
-        in_path = unquote(in_path[7:])
+        size=int(ns.size)
+    except ValueError:
+        print('invalid SIZE:',ns.size)
+        parser.print_usage()
+        return 1
 
     thumbnailer = thumbnail_tools.Thumbnailer(force_recreation=True,
                                               archive_support=True,
@@ -46,5 +65,6 @@ if __name__ == '__main__':
     thumb = thumbnailer.thumbnail(in_path)
     thumb.savev(out_path, 'png', [], [])
 
-    sys.exit(0)
+if __name__ == '__main__':
+    exit(main())
 
