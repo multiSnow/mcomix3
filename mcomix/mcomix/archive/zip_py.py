62eebb2997d803226ef6eccfc49dae831cab25a3
multiSnow <infinity.blick.winkel@gmail.com>
2019-04-09 23:15:17 +0800

    non-utf8 filename in zipfile

--- mcomix/mcomix/archive/zip_py.py
+++ mcomix/mcomix/archive/zip_py.py
@@ -2,13 +2,12 @@
 
 ''' Unicode-aware wrapper for zipfile.ZipFile. '''
 
+import collections
 import os
 import zipfile
 
 from mcomix import log
 from mcomix.archive import archive_base
-
-
 
 def is_py_supported_zipfile(path):
     '''Check if a given zipfile has all internal files stored with Python supported compression
@@ -26,30 +25,41 @@ class ZipArchive(archive_base.NonUnicodeArchive):
         super(ZipArchive, self).__init__(archive)
         self._zip = zipfile.ZipFile(archive, 'r')
 
+        # zipfile is usually not thread-safe
+        # so use OrderedDict to save ZipInfo in order
+        # {unicode_name: ZipInfo}
+        self._contents_info = collections.OrderedDict()
+        for info in self._zip.infolist():
+            if info.flag_bits & 0x800:
+                # filename is utf8 encoded
+                self._contents_info[info.filename] = info
+            else:
+                # zipfile use cp437 to decode non-utf8 filename
+                # revert to bytes before guessing encode
+                fn_bytes = info.filename.encode('cp437')
+                self._contents_info[self._unicode_filename(fn_bytes)] = info
+
         self.is_encrypted = self._has_encryption()
         self._password = None
 
     def iter_contents(self):
-        if self.is_encrypted:
+        if self.is_encrypted and not self._password:
             self._get_password()
             self._zip.setpassword(self._password)
-
-        for filename in self._zip.namelist():
-            yield self._unicode_filename(filename)
+        yield from self._contents_info.keys()
 
     def extract(self, filename, destination_dir):
         destination_path = os.path.join(destination_dir, filename)
+        info = self._contents_info[filename]
         with self._create_file(destination_path) as new:
-            content = self._zip.read(self._original_filename(filename))
-            new.write(content)
+            filelen = new.write(self._zip.read(info))
 
-        zipinfo = self._zip.getinfo(self._original_filename(filename))
-        if len(content) != zipinfo.file_size:
+        if filelen != info.file_size:
             log.warning(_('%(filename)s\'s extracted size is %(actual_size)d bytes,'
-                ' but should be %(expected_size)d bytes.'
-                ' The archive might be corrupt or in an unsupported format.'),
-                { 'filename' : filename, 'actual_size' : len(content),
-                  'expected_size' : zipinfo.file_size })
+                          ' but should be %(expected_size)d bytes.'
+                          ' The archive might be corrupt or in an unsupported format.'),
+                        {'filename': filename, 'actual_size': filelen,
+                         'expected_size': info.file_size})
         return destination_path
 
     def close(self):
@@ -58,10 +68,9 @@ class ZipArchive(archive_base.NonUnicodeArchive):
     def _has_encryption(self):
         ''' Checks all files in the archive for encryption.
         Returns True if at least one encrypted file was found. '''
-        for zipinfo in self._zip.infolist():
-            if zipinfo.flag_bits & 0x1: # File is encrypted
+        for info in self._contents_info.values():
+            if info.flag_bits & 0x1: # File is encrypted
                 return True
-
         return False
 
 # vim: expandtab:sw=4:ts=4
