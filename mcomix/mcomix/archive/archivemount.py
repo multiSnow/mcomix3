22adbae85fb53ff9ad288648032bae0bd1930279
multiSnow <infinity.blick.winkel@gmail.com>
2019-04-19 21:09:43 +0800

    ArchivemountArchive

---
+++ mcomix/mcomix/archive/archivemount.py
@@ -0,0 +1,11 @@
+import shutil
+
+from mcomix.archive import archive_base
+
+class ArchivemountArchive(archive_base.MountArchive):
+    def __init__(self,archive):
+        super(ArchivemountArchive,self).__init__(archive,'archivemount')
+
+    @staticmethod
+    def is_available():
+        return shutil.which('archivemount') and shutil.which('fusermount')
