f862a952aaee93961ecd029b9c52d4d2ad956537
multiSnow <infinity.blick.winkel@gmail.com>
2019-04-21 00:00:50 +0800

    use os.environ

--- mcomix/mcomix/tools.py
+++ mcomix/mcomix/tools.py
@@ -72,55 +72,50 @@ def bin_search(lst, value):
 
 
 def get_home_directory():
-    '''On UNIX-like systems, this method will return the path of the home
-    directory, e.g. /home/username. On Windows, it will return an MComix
-    sub-directory of <Documents and Settings/Username>.
+    '''Return the path to the MComix home directory.
+    In portable mode, it is 'profile' in the same directory with mcomixstarter.py.
+    In Windows, it is %APPDATA%/MComix if not in portable mode.
+    In UNIX, it is $HOME if not in portable mode.
     '''
     if is_portable_mode():
         # multiple profiles, maybe :)
         return os.path.join(rootdir(),'profile')
     if sys.platform == 'win32':
-        return os.path.join(os.path.expanduser('~'), 'MComix')
+        return os.path.join(os.environ.get('APPDATA'), 'MComix')
     else:
-        return os.path.expanduser('~')
+        return os.environ.get('HOME')
 
 
 def get_config_directory():
-    '''Return the path to the MComix config directory. On UNIX, this will
-    be $XDG_CONFIG_HOME/mcomix, on Windows it will be the same directory as
-    get_home_directory().
+    '''Return the path to the MComix config directory.
+    It is get_home_directory()/.config/mcomix if in portable mode.
+    If not in portable mode, it will be $XDG_CONFIG_HOME/mcomix,
+    or get_home_directory()/.config/mcomix if $XDG_CONFIG_HOME is empty.
 
     See http://standards.freedesktop.org/basedir-spec/latest/ for more
     information on the $XDG_CONFIG_HOME environmental variable.
     '''
-    if is_portable_mode():
-        # always using get_home_directory() even if in unix
-        return os.path.join(get_home_directory(),'config')
-    if sys.platform == 'win32':
-        return get_home_directory()
-    else:
-        base_path = os.getenv('XDG_CONFIG_HOME',
-            os.path.join(get_home_directory(), '.config'))
-        return os.path.join(base_path, 'mcomix')
+    prefix = os.path.join(get_home_directory(),'.config')
+    # always using get_home_directory() even if in unix
+    if not is_portable_mode():
+        prefix = os.environ.get('XDG_CONFIG_HOME',prefix)
+    return os.path.join(prefix, 'mcomix')
 
 
 def get_data_directory():
-    '''Return the path to the MComix data directory. On UNIX, this will
-    be $XDG_DATA_HOME/mcomix, on Windows it will be the same directory as
-    get_home_directory().
+    '''Return the path to the MComix data directory.
+    It is get_home_directory()/.local/share/mcomix if in portable mode.
+    If not in portable mode, it will be $XDG_DATA_HOME/mcomix,
+    or get_home_directory()/.local/share/mcomix if $XDG_DATA_HOME is empty.
 
     See http://standards.freedesktop.org/basedir-spec/latest/ for more
     information on the $XDG_DATA_HOME environmental variable.
     '''
-    if is_portable_mode():
-        # always using get_home_directory() even if in unix
-        return os.path.join(get_home_directory(),'data')
-    if sys.platform == 'win32':
-        return get_home_directory()
-    else:
-        base_path = os.getenv('XDG_DATA_HOME',
-            os.path.join(get_home_directory(), '.local/share'))
-        return os.path.join(base_path, 'mcomix')
+    prefix = os.path.join(get_home_directory(),'.local/share')
+    if not is_portable_mode():
+        prefix = os.environ.get('XDG_DATA_HOME',prefix)
+    return os.path.join(prefix, 'mcomix')
+
 
 def get_thumbnails_directory():
     '''Return the path to the thumbnail cache directory.
@@ -131,11 +126,11 @@ def get_thumbnails_directory():
     See http://standards.freedesktop.org/basedir-spec/latest/ for more
     information about the $XDG_CACHE_HOME environmental variable.
     '''
-
     prefix = os.path.join(get_home_directory(), '.cache')
     if not is_portable_mode():
         prefix = os.environ.get('XDG_CACHE_HOME', prefix)
     return os.path.join(prefix, 'thumbnails/normal')
+
 
 def number_of_digits(n):
     if 0 == n:
