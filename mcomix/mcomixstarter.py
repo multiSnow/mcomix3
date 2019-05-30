aa96b7f04b766b217aef0d39004e7f5ba076f2c9
multiSnow <infinity.blick.winkel@gmail.com>
2019-05-31 00:36:12 +0800

    show terminated message with python2

--- mcomix/mcomixstarter.py
+++ mcomix/mcomixstarter.py
@@ -18,5 +18,16 @@
 # along with this program; if not, write to the Free Software
 # -------------------------------------------------------------------------
 
-import mcomix.run
-mcomix.run.run()
+import sys
+
+py2msg='''PROGRAM TERMINATED
+Python 2.x is no longer supported.
+'''
+
+if sys.version_info.major<3:
+    sys.stderr.write(py2msg)
+    sys.exit(1)
+
+if __name__=='__main__':
+    import mcomix.run
+    mcomix.run.run()
